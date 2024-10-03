2ef3c2aecab325dfdc432fceb750fa4397fbbd89
TimotheeDag <timothee@dagany.com>
2024-10-03 11:01:04 +0200

    Clone
    Clone

---
+++ src/Controleurs/c_connexion.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Gestion de la connexion
+ *
+ * PHP Version 8
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    José GIL <jgil@ac-nice.fr>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+use Outils\Utilitaires;
+
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+if (!$uc) {
+    $uc = 'demandeconnexion';
+}
+
+switch ($action) {
+    case 'demandeConnexion':
+        include PATH_VIEWS . 'v_connexion.php';
+        break;
+    case 'valideConnexion':
+        $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $visiteur = $pdo->getInfosVisiteur($login);
+        $test = $pdo->getMdpHash($login);
+
+        $getInfo = $pdo->getInfo($login, $mdp);
+        if (!password_verify($mdp, $test)) {
+            Utilitaires::ajouterErreur('Login ou mot de passe incorrect');
+            include PATH_VIEWS . 'v_erreurs.php';
+            include PATH_VIEWS . 'v_connexion.php';
+        } else {
+            $id = $getInfo['id'];
+            $nom = $getInfo['nom'];
+            $prenom = $getInfo['prenom'];
+            Utilitaires::connecter($id, $nom, $prenom);
+            if ($pdo->getInfosComptable($login, $mdp)) {
+                include PATH_VIEWS . 'v_accueil_Compt.php';
+            } elseif ($pdo->getInfosVisiteur($login, $mdp)) {
+                header('Location: index.php');
+            }
+        }
+        break;
+    default:
+        include PATH_VIEWS . 'v_connexion.php';
+        break;
+}
